e6067fd82e1a7e124760d54e2d3ae4c115e5354f
Tea Shkurti <72191797+Tea01@users.noreply.github.com>
2022-01-21 23:08:30 +0300

    Update Dugum.cpp

--- src/Dugum.cpp
+++ src/Dugum.cpp
@@ -1,11 +1,3 @@
-/**
-* @file                         B191210555
-* @description                  .txt dosyadan verileri okuyup, ilk olarak iki nokta arasındaki uzaklığını bulup sonra her satır için noktalar arasındaki toplam uzaklığını bulup kuyruğa tutuyoruz. Sonra her noktanın orijinden uzaklığını bulup kuyrukta bağlı liste kullanarak tutuyoruz. Sonra toplam uzaklığını kullanarak her satırı AVL ağaçta post order kullanarak düzenliyoruz ve ekrana ağaçtan, orijinden uzaklığını çıkartıyoruz. 
-* @course                       1. Öğretim C Grubu
-* @assignment                   2. Ödev
-* @date                         26/12/2021
-* @author                       Tea Shkurti | E-posta: tea.shkurti@ogr.sakarya.edu.tr
-*/
 
 #include "Dugum.hpp"
 
